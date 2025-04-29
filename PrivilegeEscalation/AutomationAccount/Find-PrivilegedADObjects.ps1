fe06b6da8ced1234973c6d1b552732bc864042fb
Eduardo Barbosa (an4kein) <37910997+an4kein@users.noreply.github.com>
2025-04-29 11:35:44 -0300

    Update Find-PrivilegedADObjects.ps1

--- PrivilegeEscalation/AutomationAccount/Find-PrivilegedADObjects.ps1
+++ PrivilegeEscalation/AutomationAccount/Find-PrivilegedADObjects.ps1
@@ -1,9 +1,1 @@
-$keywords = 'admin','create','run'
-az ad signed-in-user list-owned-objects --output json | ConvertFrom-Json | Where-Object {
-    foreach ($word in $keywords) {
-        if ($_.description -match $word -or $_.displayName -match $word) {
-            return $true
-        }
-    }
-    return $false
-} | Format-Table displayName, description, objectId
+$keywords = 'admin','create','run'; az ad signed-in-user list-owned-objects --output json | ConvertFrom-Json | Where-Object { foreach ($word in $keywords) { if ($_.description -match $word -or $_.displayName -match $word) { return $true } }; return $false } | Format-Table displayName, description, objectId
